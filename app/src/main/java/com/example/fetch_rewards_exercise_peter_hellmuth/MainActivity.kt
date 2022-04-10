5871515ad7ba18200a7bc0c8af306836c3484f3d
Peter <peterhellmuth@gmail.com>
2022-04-10 10:10:13 -0500

    View Binding
    View binding setup so that kotlin can modify the TextView widget.

--- app/src/main/java/com/example/fetch_rewards_exercise_peter_hellmuth/MainActivity.kt
+++ app/src/main/java/com/example/fetch_rewards_exercise_peter_hellmuth/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.fetch_rewards_exercise_peter_hellmuth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fetch_rewards_exercise_peter_hellmuth.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val jsonString = "Test"
+
+
+        binding.mainText.text = jsonString
     }
+
 }
